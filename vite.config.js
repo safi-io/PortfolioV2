b77d71b85a114de7f25b704eee200eca6beb4aa5
safi-io <m.safi.ullah@outlook.com>
2024-08-24 10:03:24 +0500

    Trying Deploying

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolioV2/', // Set the base path to your repository name
+  base: '/portfolioV2/', // Ensure this matches your GitHub repository name
+  build: {
+    outDir: 'dist', // Default output directory, should contain the built files
+    rollupOptions: {
+      input: {
+        main: './index.html', // Entry point for the build
+      },
+    },
+  },
 });
