9799e2eafe750df74b8e9628c2ef4e4bfdcfa9d8
safi-io <m.safi.ullah@outlook.com>
2024-08-24 09:59:50 +0500

    Trying Deploying

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PortfolioV2/",  // one change 
+  base: '/portfolioV2/', // Set the base path to your repository name
 });
