00a5fcc16c1dc28e7d0a1b11018bb50f8f751bc1
safi-io <m.safi.ullah@outlook.com>
2024-08-24 10:07:23 +0500

    Will use Netlify.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolioV2/', // Ensure this matches your GitHub repository name
-  build: {
-    outDir: 'dist', // Default output directory, should contain the built files
-    rollupOptions: {
-      input: {
-        main: './index.html', // Entry point for the build
-      },
-    },
-  },
-});
+})
